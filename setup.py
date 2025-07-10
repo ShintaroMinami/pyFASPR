3a3d201b92f812127283261cbb845d50c9bb449d
Shintaro Minami <sminami@LocalMacBook.local>
2025-07-10 14:14:38 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -32,11 +32,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     setup_requires=['setuptools_scm'],
-    install_requires=[
-        'numpy',
-        'pandas',
-        'memory_tempfile',
-    ],
+    install_requires=[],
     scripts=[
         'scripts/pyfaspr'
     ],
